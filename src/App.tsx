b9c36fe60c5da05c99c35bb05558bfa116b5db00
Victor H. Watanabe <victor.watanabe@al.infnet.edu.br>
2025-03-30 16:55:59 -0300

    /components - Criação dos componentes Header e Sidebar;
    /pages
    - Criação da página Tickets;
    
    > Renomeação do css global de index para global;
    > Adição da biblioteca MUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Tickets from "./pages/Tickets/Tickets";
 
 export default function App(){
 
@@ -12,6 +13,7 @@ export default function App(){
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/tickets" element={<Tickets />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
